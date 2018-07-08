ea2af7766e5352a39735c992862d91d856c2d545
eskhancha <36293467+eskhancha@users.noreply.github.com>
2018-07-08 17:26:53 +0300

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host  = 'localhost'; // адрес сервера 
-$database = 'han64yaru2_ddsys'; // имя базы данных
-$user = 'han64yaru2_ddsys'; // имя пользователя
-$password = 'OIPJ983ej)('; // пароль 
-
-
-?>
